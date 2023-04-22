aa74bc699471cb9f9779c773f368220ab64dbf33
Nikita <fffnikitafff@gmail.com>
2023-04-22 17:07:04 +0300

    fix

--- alumni/settings.py
+++ alumni/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY = config("SECRET_KEY")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-w3h*^u@gi3h#08t(i!ex5nu&lqbfa+#$@wsb56yjgj1abhfd8t"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
