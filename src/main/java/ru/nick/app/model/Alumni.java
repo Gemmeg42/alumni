e6fa505f8073478d23c103093bb820f423c05214
Kirill Fedotov <kirill.fedotov@stentle.com>
2024-03-01 12:33:19 +0100

    18 fixed errors in db configuration

--- src/main/java/ru/nick/app/model/Alumni.java
+++ src/main/java/ru/nick/app/model/Alumni.java
@@ -1,5 +1,14 @@
 package ru.nick.app.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "alumni")
 public class Alumni {
+
+    @Id
+    public String id;
     public String name;
 }
